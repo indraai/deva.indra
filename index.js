8ebef36c096f59983cd2f9640ef82f52a8de93a2
Quinn Michaels <quinn@indra.ai>
2025-09-12 10:28:29 -0700

    indra upate deva core

--- index.js
+++ index.js
@@ -49,21 +49,7 @@ const INDRA = new Deva({
   devas: {},
   func: {},
   methods: {},
-  async onInit(data, resolve) {
-    // check license
-    const {personal} = this.license(); // get the license config
-    this.vars.license = await this.methods.license_check(personal, pkg.VLA);
-    if (this.vars.license) {
-      return this.start(data, resolve); // start load if license valid
-    }
-    else {       
-      return  this.stop(data, resolve); // stop load if license is invalid.
-    }
-  },
   async onReady(data, resolve) {
-    const {concerns, global, personal} = this.license(); // get the license config
-    this.vars.license = this.methods.license_check(personal, pkg.VLA);
-    
     this.prompt(this.vars.messages.ready);
     return resolve(data);
   },
